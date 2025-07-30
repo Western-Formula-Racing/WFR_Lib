65bf8d08efdff920de564927909db40c5ee1b296
yamaan93 <yamaan93@gmail.com>
2025-07-30 14:50:28 -0400

    i have plans that I cannot share because the haters will sabotage me

--- include/WFR_Lib/CAN.hpp
+++ include/WFR_Lib/CAN.hpp
@@ -1,36 +1,49 @@
 #ifndef __CAN__
 #define __CAN__
 
-#include <map>
 #include "driver/twai.h"
-#include "etl/vector.h"
 #include <stdio.h>
 #include "esp_log.h"
-#include <variant>
+#include <map>
+#include "freertos/semphr.h"
+#include <tuple>
 
-class CAN
-{
-public:
-    CAN(gpio_num_t CAN_TX_Pin, gpio_num_t CAN_RX_Pin, twai_mode_t twai_mode);
-    void begin();
-    bool logging;
-    int restart_counter;
-    void restart(gpio_num_t CAN_TX_Pin, gpio_num_t CAN_RX_Pin, twai_mode_t twai_mode);
-private:
-    twai_handle_t twai_handle;
-    twai_general_config_t g_config;
-    twai_timing_config_t t_config;
-    twai_filter_config_t f_config;
-    uint32_t twai_alerts;
-    uint16_t txCallBackCounter; 
+namespace CAN{
+    class Message{
+        public:
+            typedef enum{
+                OK,
+                PARSE_ERROR,
+                SERIALIZATION_ERROR,    
+            }MESSAGE_ERROR;
+            bool timed_out();
+            virtual MESSAGE_ERROR parse(uint64_t* buffer);
+            virtual MESSAGE_ERROR serialize(uint64_t* buffer);
+        private:
+            uint64_t last_recieved;
+        
+    };
+    template<class Type>
+    class Signal{
+        public:
+            typedef enum{
+                OK,
+                PARSE_ERROR,
+                SERIALIZATION_ERROR,    
+            }SIGNAL_ERROR;
+            virtual T get(); 
+            virtual SIGNAL_ERROR set(T value); 
+             
+        private:
+            T value;
+            int scale;
+            int offset;
+    };
 
-    static void rx_task_wrapper(void *arg); // Wrapper function
-    static void tx_CallBack_wrapper(TimerHandle_t xTimer); // Wrapper function
-    void tx_CallBack(); // Non-static member function
-    void rx_task(); // Non-static member function
-    
-};
+}// CAN namespace
 
-int give_zero();
+
+
+
 
 #endif
